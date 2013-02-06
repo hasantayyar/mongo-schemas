781d344d342a75bf85e90657b7d33224bceb3aad
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2013-02-06 14:42:09 +0200

    feed schema

--- activity-feed/feed.js
+++ activity-feed/feed.js
@@ -1,40 +1,50 @@
+[
+
 {
     "published": "2013-02-10T15:04:55Z",
     "actor": {
-      "url": "http://www.eba.gov.tr/profil/_n7G-OtF8GTV5Tbt2HLxlfkoDF5RZ-5Gi6Zmxpk2izs?tayyar-besik",
-      "objectType" : "person",
-      "id": 32158520130,
-      "displayName": "Tayyar BEŞİK"
+        "url": "http://www.eba.gov.tr/profil/_n7G-OtF8GTV5Tbt2HLxlfkoDF5RZ-5Gi6Zmxpk2izs?tayyar-besik",
+        "objectType": "person",
+        "id": 32158520130,
+        "displayName": "Tayyar BEŞİK"
     },
     "verb": "post",
-    "object" : {
-     "url": "http://www.eba.gov.tr/haber/1359938214",
-      "id": "haber/1359938214",
-      "objectType" : "article",
-      "image": {
+    "object": {
+        "url": "http://www.eba.gov.tr/haber/1359938214",
+        "id": "haber/1359938214",
+        "objectType": "article",
+        "image": {
             "url": "http://video.eba.gov.tr/307/015/04c/584/fda/034/a59/b75/89a/d2b/c68/f4f/2d9/1ca/001/30701504c584fda034a59b7589ad2bc68f4f2d91ca001.jpg",
             "width": 726,
             "height": 1000
-          }
+        }        
     }
-},
+}
+,
 {
     "published": "2013-01-11T10:12:00Z",
     "actor": {
-      "url": "http://www.eba.gov.tr/profil/_n7G-OtF8GTV5Tbt2HLxlfkoDF5RZ-5Gi6Zmxpk2izs?tayyar-besik",
-      "objectType" : "person",
-      "id": 32158520130,
-      "displayName": "Tayyar BEŞİK"
+        "url": "http://www.eba.gov.tr/profil/_n7G-OtF8GTV5Tbt2HLxlfkoDF5RZ-5Gi6Zmxpk2izs?tayyar-besik",
+        "objectType": "person",
+        "id": 32158520130,
+        "displayName": "Tayyar BEŞİK"
     },
     "verb": "like",
-    "object" : {
-     "url": "http://www.eba.gov.tr/gorsel/bak/0d9b8dd9d9116789549eaa3cf7fb623f03114ba89e017",
-      "id": "gorsel/bak/0d9b8dd9d9116789549eaa3cf7fb623f03114ba89e017",
-      "objectType" : "article",
-      "image": {
+    "object": {
+        "url": "http://www.eba.gov.tr/gorsel/bak/0d9b8dd9d9116789549eaa3cf7fb623f03114ba89e017",
+        "id": "gorsel/bak/0d9b8dd9d9116789549eaa3cf7fb623f03114ba89e017",
+        "objectType": "article",
+        "image": {
             "url": "http://video.eba.gov.tr/0d9/b8d/d9d/911/678/954/9ea/a3c/f7f/b62/3f0/311/4ba/89e/017/0d9b8dd9d9116789549eaa3cf7fb623f03114ba89e017.jpeg",
             "width": 1100,
             "height": 1654
-          }
+        },
+        "author": {
+            "displayName": "EBA",
+            "url": "https://www.eba.gov.tr",
+            "objectType": "person"
+        }
     }
 }
+
+]
