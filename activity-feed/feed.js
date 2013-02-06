c8e5b6c7657be0119ff3358c1551faaab7b7b1be
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2013-02-06 14:03:54 +0200

    feed schema

---
+++ activity-feed/feed.js
@@ -0,0 +1,20 @@
+{
+    "published": "2011-02-10T15:04:55Z",
+    "actor": {
+      "url": "http://www.eba.gov.tr/profil/_n7G-OtF8GTV5Tbt2HLxlfkoDF5RZ-5Gi6Zmxpk2izs?tayyar-besik",
+      "objectType" : "person",
+      "id": 32158520130,
+      "displayName": "Tayyar BEŞİK"
+    },
+    "verb": "post",
+    "object" : {
+     "url": "http://www.eba.gov.tr/haber/1359938214",
+      "id": "haber/1359938214",
+      "objectType" : "news",
+      "image": {
+            "url": "http://video.eba.gov.tr/307/015/04c/584/fda/034/a59/b75/89a/d2b/c68/f4f/2d9/1ca/001/30701504c584fda034a59b7589ad2bc68f4f2d91ca001.jpg",
+            "width": 726,
+            "height": 1000
+          }
+    }
+}
